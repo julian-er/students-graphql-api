e9217b5644b0c7f58a18c18eaf432b2283d330cb
Julián Rosalen <rosalenjulian@gmail.com>
2020-11-08 20:12:49 -0300

    Setting GraphiQL

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,9 @@
-//testing serve
-//console.log('hi, this is the online academy !')
-
 import express from "express";
 import compression from "compression";
 import cors from "cors";
-import { createServer } from "http";
-import { ApolloServer } from "apollo-server-express";
 import schema from "./schema";
-import expressPlayGround from "graphql-playground-middleware-express";
+import {graphqlHTTP} from 'express-graphql';
+
 
 const app = express();
 //settings
@@ -15,25 +11,20 @@ app.set("port", 3000);
 app.use("*", cors());
 app.use(compression());
 
-const server = new ApolloServer({
-  schema,
-  introspection: true,
-});
-
-server.applyMiddleware({ app });
 
 //define endpoint with expressPlayground
-app.get(
+app.use(
   "/",
-  expressPlayGround({
-    endpoint: "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql:true
   })
+
 );
 
-const httpServer = createServer(app);
 
 //starting server
-httpServer.listen(app.get("port"), () => {
+app.listen(app.get("port"), () => {
   console.log(`server on port http://localhost:${app.get("port")}/`);
   console.log(`server apollo on http://localhost:${app.get("port")}/graphql  `);
 });
